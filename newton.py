4d1ae0b3c4597a30981c71764eed18f0beef68ec
aaronBurns59 <aaronburns59@gmail.com>
2019-09-23 20:49:55 +0100

    Made the function more dynamic

--- newton.py
+++ newton.py
@@ -2,11 +2,15 @@
 # A program which gets the approximates the square root of numbers
 # Adpated from https://tour.golang.org/flowcontrol/8
 
-# testing how a cpu does square root function
 def sqrt(x):
     # make and initial guess as to what the square root of x is
     z = 5
-    z -= (z*z - x) / (2*z)
+    # keep getting a better estimate for the sqrt of x 
+    # until you are within 2 decimal places
+    # "abs" returns postives integers of negitive numbers
+    while abs(z*z - x >= 0.01):
+        # get a better approximation for the sqrt
+        z -= (z*z - x) / (2*z)
     return z
 
-print(sqrt(25))
+print(sqrt(8.0))
