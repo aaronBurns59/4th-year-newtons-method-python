bb46d5e95bda56264539a2315d1ffb1475c8a233
aaronBurns59 <aaronburns59@gmail.com>
2019-09-23 21:02:32 +0100

    Fixed error in the function

--- newton.py
+++ newton.py
@@ -4,13 +4,17 @@
 
 def sqrt(x):
     # make and initial guess as to what the square root of x is
-    z = 5
+    z = 1.0
     # keep getting a better estimate for the sqrt of x 
     # until you are within 2 decimal places
     # "abs" returns postives integers of negitive numbers
-    while abs(z*z - x >= 0.01):
+    while abs(z*z - x) >= 0.01:
         # get a better approximation for the sqrt
         z -= (z*z - x) / (2*z)
     return z
-
-print(sqrt(8.0))
+# print the sqrt of number in function
+z = sqrt(64.0)
+# print z
+print(z)
+# print the sqrt of the sqrt z
+print(z*z)
